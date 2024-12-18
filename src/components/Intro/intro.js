34490b42bef94970a8f0ae238275baa7915b8f70
Pushpa-Harshitha05 <143336597+Pushpa-Harshitha05@users.noreply.github.com>
2024-12-18 20:13:01 +0530

    added next section in portfolio

---
+++ src/components/Intro/intro.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import './intro.css'
+import profile from '../../assests/profile.png'
+import {useState,useEffect} from "react";
+
+const Intro = () => {
+
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const toRotate = ["Web Developer", "Passionate Programmer"];
+  const typingSpeed = 130;
+  const deletingSpeed = 70;
+  const pauseTime = 1200;
+
+  useEffect(() => {
+    const ticker = setTimeout(() => tick(), isDeleting ? deletingSpeed : typingSpeed);
+    return () => clearTimeout(ticker);
+  }, [text, isDeleting]);
+
+  const tick = () => {
+    const i = loopNum % toRotate.length;
+    const fullText = toRotate[i];
+
+    if (isDeleting) {
+      setText((prev) => prev.slice(0, -1));
+      if (text === "") {
+        setIsDeleting(false);
+        setLoopNum((prev) => prev + 1);
+      }
+    } else {
+      setText((prev) => fullText.slice(0, prev.length + 1));
+      if (text === fullText) {
+        setTimeout(() => setIsDeleting(true), pauseTime);
+      }
+    }
+  };
+
+  return (
+    <section id="intro">
+      <div className="sectioncontent">
+        <span className="hello">Hello,</span>
+        <span className="introtext">I'm <span className="introname">Pushpa Harshitha</span></span>
+        <div className="titles">
+          <span className="changetitle">{text}</span>
+          <div className="tick"></div>
+        </div>
+        <div className="introbtn">
+          <a href="https://chatgpt.com/" target="_blank" rel="noopener noreferrer" className="hireme">Hire me</a>
+          <a href="https://chatgpt.com/" target="_blank" rel="noopener noreferrer" className="resume">Resume</a>
+          </div>
+      </div>
+      <img src={profile} alt="profile" className="profilepic" />
+    </section>
+  )
+}
+
+export default Intro;
